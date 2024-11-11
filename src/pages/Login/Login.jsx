cf77b7a04aae0404ce3aacada946361552cae08f
Bhavesh Sengunthar <bhaveshss012@gmail.com>
2024-11-11 21:54:29 +0530

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <Button>Shad Cn Button !!</Button>
+    </div>
+  );
+};
+
+export default Login;
